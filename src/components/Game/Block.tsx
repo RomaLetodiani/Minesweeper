01aef632e5a2f44331c397acdae49d569a49061c
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-25 03:57:22 +0400

    Update Block.tsx

--- src/components/Game/Block.tsx
+++ src/components/Game/Block.tsx
@@ -1,24 +1,34 @@
 import { useGame } from '../../Contexts/GameContext'
-import { flagBlock } from '../../utils/FlagBlock'
 import { BlockProps } from './Interfaces'
 
+const mines = [
+  '',
+  'text-mines-1',
+  'text-mines-2',
+  'text-mines-3',
+  'text-mines-4',
+  'text-mines-5',
+  'text-mines-6',
+  'text-mines-7',
+  'text-mines-8',
+]
 const Block = ({ hasMine, revealed, surroundingMines, flagged, row, col }: BlockProps) => {
-  const { gameBoard, setGameBoard, gameState, setGameState } = useGame()
+  const { BlockOnClick } = useGame()
   return (
     <div
-      className="relative flex justify-center items-center border border-slate-800 w-[clamp(20px,5vw,24px)] h-[clamp(20px,5vw,24px)] font-extrabold"
-      onContextMenu={(e) =>
-        flagBlock(e, gameBoard, setGameBoard, row, col, gameState, setGameState)
-      }
-      onClick={(e) => flagBlock(e, gameBoard, setGameBoard, row, col, gameState, setGameState)}
+      className="relative flex justify-center items-center border border-slate-800 w-[clamp(24px,5vw,28px)] h-[clamp(24px,5vw,28px)] font-extrabold"
+      onContextMenu={(e) => BlockOnClick(e, row, col)}
+      onClick={(e) => BlockOnClick(e, row, col)}
     >
-      <span className=" select-none">
+      <span className="select-none">
         {hasMine && revealed ? (
           '💥'
         ) : hasMine ? (
           '💣'
         ) : (
-          <span className={`${!surroundingMines && 'opacity-0'}`}>{surroundingMines}</span>
+          <span className={`${!surroundingMines && 'opacity-0'} ${mines[surroundingMines]}`}>
+            {surroundingMines}
+          </span>
         )}
       </span>
       {!revealed && (
