5a76753d89f4483489352199afab50aa5cc79f10
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-24 21:06:55 +0400

    Create Root.tsx

---
+++ src/router/Root.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import Header from '../Layout/Header'
+
+const Root = () => {
+  return (
+    <div className="min-h-screen min-w-96 flex flex-col">
+      <Header />
+      <Outlet />
+    </div>
+  )
+}
+
+export default Root
