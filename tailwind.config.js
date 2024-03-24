f12e7afc24d385678d0df11d0ab2c9b5a3ce05e5
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-25 03:58:47 +0400

    added text colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'mines-1': '#990000',
+        'mines-2': '#006699',
+        'mines-3': '#660000',
+        'mines-4': '#006600',
+        'mines-5': '#663300',
+        'mines-6': '#003300',
+        'mines-7': '#003366',
+        'mines-8': '#660033',
+      },
       boxShadow: {
         Block: 'inset 3px 3px 5px #bababa, inset -3px -3px 5px #ffffff',
         Blocks: '0px 0px 20px -5px #5f6984',
