1cfff664be1a4358dfc6d4c033d92506c9cf6879
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-27 14:26:39 +0400

    Create GlobalIcons.tsx

---
+++ src/components/icons/GlobalIcons.tsx
@@ -0,0 +1,11 @@
+export const CloseIcon = (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M15 5L5 15M5 5L15 15"
+      stroke="#fff"
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
