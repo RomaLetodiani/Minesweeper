a643b0daaddb8551d6482d39d026d3c79c3bcf76
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-27 14:26:20 +0400

    Update Header.tsx

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,26 +1,89 @@
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import logo from '../assets/logo.png'
-import useCountdownTimer from '../hooks/useCountDownTimer'
 import { formatTime } from '../utils/formatTime'
 import { useGame } from '../Contexts/GameContext'
+import useTimer from '../hooks/useTimer'
+import { useEffect } from 'react'
+import Modal from '../components/Modal'
+import useMediaQuery from '../hooks/useMediaQuery'
 const Header = () => {
   const { mode } = useParams()
-  const { gameState } = useGame()
-  const { seconds } = useCountdownTimer(
-    mode === 'easy' ? 1200 : mode === 'intermediate' ? 900 : 600,
-  )
+  const { gameState, setGameState, resetGame } = useGame()
+  const { score, FlaggedBlocks, mines } = useGame()
+  const { seconds, resetTimer, isRunning, startTimer, pauseTimer } = useTimer()
+  const isDesktop = useMediaQuery('(min-width: 1025px)')
+
+  useEffect(() => {
+    resetTimer()
+    resetGame()
+  }, [mode, isDesktop])
+
+  useEffect(() => {
+    if (gameState.Over || gameState.win) {
+      pauseTimer()
+    }
+  }, [gameState.Over, gameState.win])
+
+  const handlePause = () => {
+    if (isRunning) {
+      pauseTimer()
+    } else {
+      startTimer()
+    }
+
+    setGameState((prev) => ({ ...prev, Paused: isRunning }))
+  }
+
   const time = formatTime(seconds)
   return (
-    <div className={`flex ${mode && 'justify-between'} justify-center items-center shadow-xl p-5`}>
+    <div
+      className={`flex ${mode && 'justify-between'} bg-[#333333] justify-center items-center p-5`}
+    >
       {!mode && (
         <div className="max-w-lg">
-          <img className="w-full h-full" src={logo} alt="Minesweeper" />
+          <img className="w-full h-full invert-[1] brightness-90" src={logo} alt="Minesweeper" />
         </div>
       )}
       {mode && (
         <>
+          {(gameState.Paused || gameState.Over || gameState.win) && (
+            <Modal>
+              <p>
+                {gameState.Over
+                  ? 'Game Over'
+                  : gameState.Paused
+                  ? 'Game Paused'
+                  : 'Congratulations'}
+              </p>
+              {gameState.Paused && <button onClick={handlePause}>Continue</button>}
+              {<p>Your score: {score}</p>}
+              <p>Time: {time}</p>
+              <Link to={'/'}>
+                <button
+                  onClick={() => {
+                    resetTimer()
+                    resetGame()
+                  }}
+                >
+                  Home
+                </button>
+              </Link>
+
+              <button
+                onClick={() => {
+                  resetTimer()
+                  resetGame()
+                }}
+              >
+                Reset
+              </button>
+            </Modal>
+          )}
+          <div>
+            score: {score} -------- Flags:{mines - FlaggedBlocks}
+          </div>
           <div>{time}</div>
-          <div>{gameState.score}</div>
+          <div onClick={handlePause}>pause</div>
         </>
       )}
     </div>
