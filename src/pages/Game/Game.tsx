91053a9520485e344c183832abd8706561e2ad5c
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-28 19:15:38 +0400

    added random logic

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -7,28 +7,52 @@ import useMediaQuery from '../../hooks/useMediaQuery'
 const Game = () => {
   const { mode } = useParams()
   const isDesktop = useMediaQuery('(min-width: 1025px)')
-  const { gameBoard, setRow, setCol, setMines } = useGame()
+  const { gameBoard, row, col, setRow, setCol, setMines } = useGame()
+
+  // Generate random rows and cols for random mode
+  const generateRandomDimensions = () => {
+    const minRows = 9
+    const maxRows = isDesktop ? 15 : 40
+    const minCols = 9
+    const maxCols = isDesktop ? 40 : 15
+
+    setRow(Math.floor(Math.random() * (maxRows - minRows + 1)) + minRows)
+    setCol(Math.floor(Math.random() * (maxCols - minCols + 1)) + minCols)
+  }
 
   useEffect(() => {
-    setRow(() => {
-      return mode === 'easy' ? 9 : isDesktop || mode === 'intermediate' ? 15 : 40
-    })
-    setCol(() => {
-      return mode === 'easy' ? 9 : isDesktop && mode === 'expert' ? 40 : 15
-    })
-    setMines(() => {
-      return mode === 'easy' ? 10 : mode === 'intermediate' ? 40 : 99
-    })
+    if (mode === 'random') {
+      generateRandomDimensions()
+    } else {
+      setRow(() => {
+        return mode === 'easy' ? 9 : isDesktop || mode === 'intermediate' ? 15 : 40
+      })
+      setCol(() => {
+        return mode === 'easy' ? 9 : isDesktop && mode === 'expert' ? 40 : 15
+      })
+      setMines(() => {
+        return mode === 'easy' ? 10 : mode === 'intermediate' ? 40 : 99
+      })
+    }
   }, [isDesktop])
 
-  // easy 9x9
-  // intermediate 15x15
-  // expert 40x15
+  // Logic for setting mines based on difficulty or random mode
+  useEffect(() => {
+    if (mode === 'random') {
+      const totalCells = row * col
+      const numMines = Math.floor(totalCells * 0.15) // Adjust this for desired difficulty
+      setMines(numMines)
+    }
+  }, [row, col, mode])
 
+  // easy 9 x 9
+  // intermediate 15 x 15
+  // expert 40 x 15
+  // random ? x ?
   return (
-    <div className="flex bg-[#1c1c1c] justify-center items-center flex-1 p-5">
+    <div className="flex bg-slate-800  justify-center items-center flex-1 p-5">
       <div
-        className={`shadow-[0px_0px_20px_-5px_#ffffffff] overflow-hidden rounded-xl border border-slate-800 `}
+        className={`shadow-[0px_0px_30px_-5px_#1e293b] overflow-hidden rounded-xl border border-slate-800 `}
       >
         {gameBoard?.map((r, index) => (
           <Row key={index} Blocks={r} row={index} />
