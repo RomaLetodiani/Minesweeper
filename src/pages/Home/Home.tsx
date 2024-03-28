baa096b7965c190211e6645e88f543efd81925f0
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-28 19:15:22 +0400

    added random

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,17 +2,17 @@ import Mode from '../../components/Home/Mode'
 
 const Home = () => {
   return (
-    <div className="bg-[#1c1c1c] flex-1 flex justify-center items-center">
+    <div className="bg-slate-800 flex-1 flex justify-center items-center">
       <section className="flex flex-col items-center justify-center md:flex-row gap-10 m-auto">
         <div className="flex flex-1 justify-center items-center">
           <Mode mode="easy" />
         </div>
         <div className="flex flex-col flex-1 gap-10 justify-between items-center">
           <Mode mode="intermediate" />
-          <Mode mode="random" />
+          <Mode mode="expert" />
         </div>
         <div className="flex flex-1 justify-center items-center">
-          <Mode mode="expert" />
+          <Mode mode="random" />
         </div>
       </section>
     </div>
