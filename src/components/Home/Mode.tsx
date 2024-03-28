466fb729b2834d1ec6fc2d12beea73086c1f7267
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-28 19:15:47 +0400

    fixed easy error

--- src/components/Home/Mode.tsx
+++ src/components/Home/Mode.tsx
@@ -4,7 +4,7 @@ import { CloseIcon } from '../icons/GlobalIcons'
 const Mode = ({ mode }: { mode: string }) => {
   return (
     <Link to={`/game/${mode}`}>
-      <div className="p-5 bg-[#757575] w-44 h-24 text-white rounded-xl flex justify-center items-center flex-col">
+      <div className="p-5 bg-slate-500 w-44 h-24 text-white shadow-Block rounded-xl flex justify-center items-center flex-col">
         <h2 className="font-bold uppercase text-lg">{mode}</h2>
         <p className="flex justify-center items-center gap-3">
           <span>
@@ -12,7 +12,7 @@ const Mode = ({ mode }: { mode: string }) => {
           </span>
           {CloseIcon}
           <span>
-            {mode === 'Easy' ? 9 : mode === 'intermediate' ? 15 : mode === 'expert' ? 40 : '?'}
+            {mode === 'easy' ? 9 : mode === 'intermediate' ? 15 : mode === 'expert' ? 40 : '?'}
           </span>
         </p>
       </div>
