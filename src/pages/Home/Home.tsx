2cb9138fbe4ef0e484b9d049a849afa3edaba4bc
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-30 18:53:27 +0400

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,21 @@
 import Mode from '../../components/Home/Mode'
+import useMediaQuery from '../../hooks/useMediaQuery'
 
 const Home = () => {
+  const isMobile = useMediaQuery('(min-width: 768px)')
+
   return (
-    <div className="bg-slate-800 flex-1 flex justify-center items-center">
-      <section className="flex flex-col items-center justify-center md:flex-row gap-10 m-auto">
+    <div className="bg-slate-950 flex-1 flex justify-center items-center p-5">
+      <section className="flex flex-col items-center justify-center md:flex-row gap-12 md:gap-16 m-auto">
         <div className="flex flex-1 justify-center items-center">
           <Mode mode="easy" />
         </div>
-        <div className="flex flex-col flex-1 gap-10 justify-between items-center">
-          <Mode mode="intermediate" />
-          <Mode mode="expert" />
+        <div className="flex flex-col flex-1 gap-12 md:gap-24 justify-between items-center">
+          <Mode mode="amateur" />
+          <Mode mode={isMobile ? 'random' : 'expert'} />
         </div>
         <div className="flex flex-1 justify-center items-center">
-          <Mode mode="random" />
+          <Mode mode={!isMobile ? 'random' : 'expert'} />
         </div>
       </section>
     </div>
