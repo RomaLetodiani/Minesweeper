94a0ced0c80faa36a38414888f8bf539e7ec5e0e
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-30 18:53:20 +0400

    Update formatTime.ts

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,17 +1,12 @@
-export const formatTime = (totalSeconds: number): string => {
+export const formatTime = (totalSeconds: number) => {
   const hours = Math.floor(totalSeconds / 3600) // Calculate hours
   const minutes = Math.floor((totalSeconds % 3600) / 60) // Calculate minutes
-  const seconds = totalSeconds % 60 // Calculate seconds
+  const Seconds = totalSeconds % 60 // Calculate seconds
 
   // Pad the hours, minutes, and seconds with leading zeros if needed
   const formattedHours = String(hours).padStart(2, '0')
   const formattedMinutes = String(minutes).padStart(2, '0')
-  const formattedSeconds = String(seconds).padStart(2, '0')
+  const formattedSeconds = String(Seconds).padStart(2, '0')
 
-  // Construct the formatted time string
-  if (hours > 0) {
-    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`
-  } else {
-    return `${formattedMinutes}:${formattedSeconds}`
-  }
+  return { formattedHours, formattedMinutes, formattedSeconds, hours, minutes, Seconds }
 }
