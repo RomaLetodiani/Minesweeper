25dd51b213d4ba995ae246142f8333af8d0482f2
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-30 18:57:17 +0400

    Update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,7 +4,9 @@ const Modal = ({ children }: { children: ReactNode }) => {
   return (
     <div className="fixed inset-0 w-full h-full flex items-center justify-center z-40">
       <div className="absolute inset-0 w-full h-full bg-[#00000080] backdrop-blur-[2px]"></div>
-      <div className="relative bg-white p-5 rounded-xl shadow-md">{children}</div>
+      <div className="relative w-80 bg-slate-950 border shadow-Block px-5 py-8 rounded-xl">
+        {children}
+      </div>
     </div>
   )
 }
